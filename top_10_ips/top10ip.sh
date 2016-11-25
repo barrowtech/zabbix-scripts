a3f6cb5f00f8627b6ed88650e0d040f784292960
Scott Barrow <scottb@redfishgroup.co.uk>
2016-11-25 13:34:50 +0000

    Addition of Top IP stat to view on graph (shows hits vs top ip) to detect Penn tests / ddos

--- top_10_ips/top10ip.sh
+++ top_10_ips/top10ip.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-topip=$(awk -vDate=`date -d'now-1 hours' +[%d/%b/%Y:%H:%M:%S` ' { if ($4 > Date) print $1}' /var/log/nginx/hbbe-access_log | sort | uniq -c | sort -nr | head -n 10)
+top10ip=$(awk -vDate=`date -d'now-1 hours' +[%d/%b/%Y:%H:%M:%S` ' { if ($4 > Date) print $1}' /var/log/nginx/hbbe-access_log | sort | uniq -c | sort -nr | head -n 10)
 hitsperhour=$(awk -vDate=`date -d'now-1 hours' +[%d/%b/%Y:%H:%M:%S` ' { if ($4 > Date) print $4}' /var/log/nginx/hbbe-access_log | cut -d: -f1 | sort -n | uniq -c)
-
-echo "'$topip'"
+topip=$(awk -vDate=`date -d'now-1 hours' +[%d/%b/%Y:%H:%M:%S` ' { if ($4 > Date) print $1}' /var/log/nginx/hbbe-access_log | sort | uniq -c | sort -nr | head -n 1)
+echo "'$top10ip'"
 echo $hitsperhour
-
+echo $topip
 zabbix_sender -z zabbix.barrowtech.com -s "hb-web1" -k "topips" -o "'$topip'"
 zabbix_sender -z zabbix.barrowtech.com -s "hb-web1" -k "hitsperhour" -o $hitsperhour
+zabbix_sender -z zabbix.barrowtech.com -s "hb-web1" -k "topip" -o $topip
+
